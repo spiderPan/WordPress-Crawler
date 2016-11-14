b0cee7462d7b59a0057063ea6d527cf429025358
pan <panbanglanfeng@gmail.com>
2016-11-13 22:43:28 -0500

    rework scan specific plugin in all sites part

--- py_crawler.py
+++ py_crawler.py
@@ -2,18 +2,21 @@
 
 import argparse
 import re
-import urllib2
-import gzip
+import requests
 import StringIO
 from bs4 import BeautifulSoup
 import os
 
 
 def _main():
-    argParser = argparse.ArgumentParser(description='Scan any wordpress powered website and identify plugins installed')
-    argParser.add_argument('-s', '--scan', metavar='<website url>', dest='url', help='scan website at <website url>')
-    argParser.add_argument('-u', '--update', type=int, metavar='<page number>', dest='pageN', help='update the list of plugins from wordpress.org up to <page number>')
-    argParser.add_argument('-a', '--all', metavar='<plugin name>', dest='pluginName', help='scaning all preset sites to look for <plugin name>')
+    argParser = argparse.ArgumentParser(
+        description='Scan any wordpress powered website and identify plugins installed')
+    argParser.add_argument('-s', '--scan', metavar='<website url>', dest='url',
+                           help='scan website at <website url> to see how many plugins installed(untested)')
+    argParser.add_argument('-u', '--update', type=int, metavar='<page number>', dest='pageN',
+                           help='update the list of plugins from wordpress.org up to <page number>')
+    argParser.add_argument('-a', '--all', metavar='<plugin name>', dest='pluginName',
+                           help='scaning all preset sites to look for <plugin name>')
     args = argParser.parse_args()
     try:
         if args.url == args.pageN == args.pluginName is None:
@@ -49,39 +52,21 @@ def _parseHrefs(html):
 
 
 def _requestURL(url):
-    headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-               "Accept-Encoding": "gzip, deflate, sdch",
-               "Accept-Language": "en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2",
-               "Cache-Control": "no-cache",
-               "Connection": "keep-alive",
-               "Pragma": "no-cache",
-               "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.124 Safari/537.36"}
 
-    request = urllib2.Request(url, headers=headers)
-    try:
-        req = urllib2.urlopen(request)
-        return req
-    except urllib2.HTTPError as e:
-        if e.code:
-            return e
-        else:
-            print u'**ERROR**: {0} : {1}\t--IGNORE--' .format(url, e)
-            return False
-    except urllib2.URLError as e:
-        print u'**ERROR**: {0} is not available: {1} \t--IGNORE--' .format(url, e)
-        return False
+    request = requests.get(url, headers=headers)
+    return request
+
+
+def _getRequestStatusCode(url):
+    response = _requestURL(url)
+    return response.status_code
 
 
 def _readURL(url):
     response = _requestURL(url)
-    if response.info().get('Content-Encoding') == 'gzip':
-        content = response.read()
-        data = StringIO.StringIO(content)
-        gzipper = gzip.GzipFile(fileobj=data)
-        html = gzipper.read()
-        return html
-    html = response.read()
-    return html
+    return response.text
 
 
 def _writePlugins(pluginsList):
@@ -92,24 +77,19 @@ def _writePlugins(pluginsList):
 
 
 def scanSites(pluginName):
-    print 'Scanning all sites for {0}...'.format(pluginName)
-    currentDir = os.path.dirname(os.path.realpath(__file__)) + os.path.sep
-    siteURLs = open(currentDir + 'sites.txt', 'r')
-    for url in siteURLs.read().split('\n'):
-        url = url.strip()
-        if not url.startswith("#"):
-            pluginURL = 'http://' + url + '/wp-content/plugins/' + pluginName + '/'
-            code = _requestURL(pluginURL).code	
-            #print code			
-            if code is False:
-                continue
-            elif code in (403,304):
-				print url + '[+]'
-            else:
-				result = _readURL(pluginURL).strip()
-				standard = 'Viewing directory contents is not permitted.'
-				if (standard.split(' ') == result.split(' ')):
-					print url + '[+]'
+    headerContent = 'Scanning all sites for {0}...'.format(pluginName)
+    resultFile = open('./results.txt', 'w+')
+    print headerContent
+    resultFile.write(headerContent + '\n')
+    urlList = [line.rstrip() for line in open('./sites.txt')]
+    for url in urlList:
+        pluginURL = 'http://' + url + '/wp-content/plugins/' + pluginName
+        code = _getRequestStatusCode(pluginURL)
+        if code in (403, 304, 200):
+            lineResultsText = url + '[+]'
+            resultFile.write(lineResultsText + '\n')
+            print lineResultsText
+    f.close()
     print "Scanning Complete!"
 
 
@@ -138,13 +118,16 @@ def update(pageN):
     elif pageN == 2:
         html = _readURL('https://wordpress.org/extend/plugins/browse/popular/')
         pluginsList = _parseHrefs(html)
-        html = _readURL('https://wordpress.org/extend/plugins/browse/popular/page/2/')
+        html = _readURL(
+            'https://wordpress.org/extend/plugins/browse/popular/page/2/')
         pluginsList = pluginsList + _parseHrefs(html)
     else:
         for page in range(2, pageN):
-            html = _readURL('https://wordpress.org/extend/plugins/browse/popular/page/' + str(page) + '/')
+            html = _readURL(
+                'https://wordpress.org/extend/plugins/browse/popular/page/' + str(page) + '/')
             pluginsList = pluginsList + _parseHrefs(html)
     _writePlugins(pluginsList)
 
 
-if __name__ == "__main__": _main()
+if __name__ == "__main__":
+    _main()
